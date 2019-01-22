38f324bd25f8d8f2f440060698cddba1d547040c
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-22 15:43:40 -0600

    added a init method

--- CustomView/app/src/main/java/com/thadocizn/customview/VolumeControlView.java
+++ CustomView/app/src/main/java/com/thadocizn/customview/VolumeControlView.java
@@ -1,24 +1,61 @@
 package com.thadocizn.customview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class VolumeControlView extends View {
+    private static final int STROKE = 1;
+    protected Paint paintOuter, paintInner, paintKnob;
+    protected int currentSetting, rotation;
+
     public VolumeControlView(Context context) {
         super(context);
+        init(null);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 
     public VolumeControlView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public VolumeControlView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
 
     public VolumeControlView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    protected void init(AttributeSet attrs){
+        paintInner = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintKnob  = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintOuter = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        paintOuter.setStyle(Paint.Style.FILL);
+        paintKnob.setStyle(Paint.Style.FILL);
+
+        paintInner.setStyle(Paint.Style.STROKE);
+        paintInner.setStrokeWidth(STROKE);
+
+        if (attrs != null){
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.VolumeControlView);
+            paintInner.setColor(typedArray.getColor(R.styleable.VolumeControlView_inner_circle, Color.BLUE));
+            paintKnob.setColor(typedArray.getColor(R.styleable.VolumeControlView_inner_circle, Color.WHITE));
+            paintOuter.setColor(typedArray.getColor(R.styleable.VolumeControlView_inner_circle, Color.RED));
+
+        }
     }
 }
