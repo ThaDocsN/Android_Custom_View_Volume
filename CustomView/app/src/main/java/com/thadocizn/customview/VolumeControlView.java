92507a2cfa6734cdc660a636492eabe298a84cec
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-22 14:56:15 -0600

    added a custom view

---
+++ CustomView/app/src/main/java/com/thadocizn/customview/VolumeControlView.java
@@ -0,0 +1,24 @@
+package com.thadocizn.customview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class VolumeControlView extends View {
+    public VolumeControlView(Context context) {
+        super(context);
+    }
+
+    public VolumeControlView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VolumeControlView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public VolumeControlView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
